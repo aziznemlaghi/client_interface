5002b0b59984cae734cd72ed2f52355bdcda72f3
azizn <aziznemlaghi20@gmail.com>
2022-04-15 14:53:30 +0100

    ioni

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '../auth/models/user.model';
 import { AuthService } from '../auth/services/auth.service';
+import { getToken } from '../helper/getToken';
 
 @Component({
   selector: 'app-tab3',
@@ -8,13 +10,29 @@ import { AuthService } from '../auth/services/auth.service';
 })
 export class Tab3Page {
 
-  constructor(    private authService: AuthService,
-    ) {}
+  user : any;
 
-    async onSignOut() {
-      
-      this.authService.logout();
+  
+
+  constructor(    private authService: AuthService,
+    ) {
+
+      this.setUser();
     }
 
+    
+
+    async onSignOut() {
+      this.authService.logout();
+    }
+    
+
+    setUser (){
+      this.user = this.authService.decodeToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InBob25lIjoxODE4MTgxOCwibmFtZSI6ImJydW5vIiwiZW1haWwiOiJicnVub0BnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE2NTAwMjc3MjksImV4cCI6MTY1MDAzMTMyOX0.jQNmqraZBk5mAiqSeaImFMaTQGwOn7hnvPv2sBUyS6c')
+    console.log('******',this.user);
+    }
+
+    /**decodeToken = (token: string): User | any =>
+    token ? jwt_decode(token) : null;*/
 
 }
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -11,18 +11,18 @@ const routes: Routes = [
       {
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule),
-        /**canLoad: [AuthGuard],*/
+        canLoad: [AuthGuard],
 
       },
       {
         path: 'tab2',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule),
-        /**canLoad: [AuthGuard],*/
+        canLoad: [AuthGuard],
       },
       {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule),
-        /**canLoad: [AuthGuard],*/
+        canLoad: [AuthGuard],
       },
       {
         path: '',
